dd49d9dbb5ebc9323f3881fb168bd7f287f8c223
gagoxyz <dgagoc@gmail.com>
2025-12-16 19:58:55 +0100

    Nueva ApiKey proyecto Firebase

--- angular/src/app/environments/environment.ts
+++ angular/src/app/environments/environment.ts
@@ -1,11 +1,12 @@
 export const environment = {
   firebaseConfig: {
-    projectId: "equipo-basket",
-    appId: "1:51765152514:web:6e25113e8bacca8103501b",
-    storageBucket: "equipo-basket.firebasestorage.app",
-    apiKey: "AIzaSyDEeYKxOUt8e7i64luTI1Z2EwdxnaWgVAY",
-    authDomain: "equipo-basket.firebaseapp.com",
-    messagingSenderId: "51765152514"
+    apiKey: "AIzaSyA-by8O6uYEsw5vPq9I-s_d_RLBZbstMQY",
+    authDomain: "angular-react-enjoyers-p2.firebaseapp.com",
+    projectId: "angular-react-enjoyers-p2",
+    storageBucket: "angular-react-enjoyers-p2.firebasestorage.app",
+    messagingSenderId: "87430062589",
+    appId: "1:87430062589:web:892dbfae8732fc24673994",
+    measurementId: "G-6DZWR6V784"
   },
   production: false
 };
